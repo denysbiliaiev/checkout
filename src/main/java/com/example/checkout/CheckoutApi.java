c8b069b7e8e36169ec6a2cf02befd23c49f3517f
denys.biliaiev <denys.biliaiev@gmail.com>
2023-12-03 14:32:32 +0100

    Spring Initializr

---
+++ src/main/java/com/example/checkout/CheckoutApi.java
@@ -0,0 +1,13 @@
+package com.example.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutApi.class, args);
+	}
+
+}
