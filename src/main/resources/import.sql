99eecb6ff3f4e2696cb2d3dd5ceaa2eae044a4eb
denys.biliaiev <denys.biliaiev@gmail.com>
2023-12-04 19:33:20 +0100

    Add hibernate.ddl-auto=create and import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO product_discount (id, count, price) VALUES(1, 3, 2000.10);
+INSERT INTO product_discount (id, count, price) VALUES(2, 2, 120.00);
+
+INSERT INTO product(id, name, price, product_discount_id) VALUES(1, 'Rolex Watch', 1000.01, 1);
+INSERT INTO product(id, name, price, product_discount_id) VALUES(2, 'Michael Kors Purse', 80.00, null);
+INSERT INTO product(id, name, price, product_discount_id) VALUES(3, 'iPhone XS', 400.00, 2);
+INSERT INTO product(id, name, price, product_discount_id) VALUES(4, 'Casio Watch', 30.40, null);
+
